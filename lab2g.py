962d63e38940a4c91cc8a101f15a76facc7574ac
Pujan Shrestha <pshrestha24@myseneca.ca>
2025-01-23 13:03:02 -0500

    Added lab2g.py

---
+++ lab2g.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Author: Your Full Name
+# Author ID: Your Seneca ID
+# Date Created: yyyy/mm/dd
+
+import sys
+
+if len(sys.argv) > 1:
+    timer = int(sys.argv[1])
+else:
+    timer = 3  # Default value if no argument is given
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
