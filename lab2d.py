ed3361ae012c6c569e3025639bd348214777df54
Pujan Shrestha <pshrestha24@myseneca.ca>
2025-01-23 13:19:18 -0500

    Added lab2d.py

--- lab2d.py
+++ lab2d.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-a = 10
-b = 10
 
-if a > b:
-    print('a is greater than b')
-elif a < b:
-    print('b is greater than a')
-else:
-    print('a is not greater than b')
-    print('a is not less than b')
-    print('Therefore, a is equal to b')
+import sys
+
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} name age")
+    sys.exit()
+
+name = sys.argv[1]
+age = sys.argv[2]
+
+print(f"Hi {name}, you are {age} years old.")
+
